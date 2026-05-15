2e4252678fb5640dd142e48b5b49acdba314f3de
jecartechnologys-web <jecartechnologys@gmail.com>
2026-05-14 21:26:06 -0500

    Fix: add Firebase domains to CSP connect-src

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const securityHeaders = [
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: blob: https://res.cloudinary.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "connect-src 'self' https://api.telegram.org https://www.google-analytics.com https://www.googletagmanager.com",
+      "connect-src 'self' https://api.telegram.org https://www.google-analytics.com https://www.googletagmanager.com https://identitytoolkit.googleapis.com https://firestore.googleapis.com https://apis.google.com",
       "frame-src 'self' https://www.google.com https://www.gstatic.com",
       "object-src 'none'",
       "base-uri 'self'",
